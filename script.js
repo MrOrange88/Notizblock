63143078efa8d96aa15948dab16d279a8423b98d
Mr.Orange88 <m.orange@gmx.de>
2025-11-28 11:56:10 +0100

    Add initial template.js file to scripts directory more javascript functions and style

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let notesTitles = ["Lebensmittel", "Aufgabe", "Aufgabe"];
 let notes = ["Banane", "Auto waschen", "einkaufen gehen"];
+
+let trashNotesTitles = [];
 let trashNotes = [];
+
+let archiveNotesTitles = [];
+let archiveNotes = [];
 
 function renderNotes() {
   let contentRef = document.getElementById("content");
@@ -19,11 +25,22 @@ function renderTrashNotes() {
 }
 
 function getNoteTemplate(indexNote) {
-  return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">X</button></p>`;
+  return `<p>${notesTitles[indexNote]} -> ${notes[indexNote]} <button class="btn" onclick="noteToTrash(${indexNote})">X</button></p>`;
 }
 
 function getTrashNoteTemplate(indexTrashNote) {
-  return `<p>+ ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">X</button></p>`;
+  return `<p>${trashNotesTitles[indexTrashNote]} -> ${trashNotes[indexTrashNote]} <button class="btn" onclick="deleteNote()">X</button></p>`;
+}
+
+function addNoteTitle() {
+  let noteTitleInputRef = document.getElementById("noteTitle_input");
+  let noteTitleInput = noteTitleInputRef.value;
+
+  notesTitles.push(noteTitleInput);
+
+  renderNotes();
+
+  noteTitleInputRef.value = "";
 }
 
 function addNote() {
@@ -37,9 +54,19 @@ function addNote() {
   noteInputRef.value = "";
 }
 
-function deleteNote(indexNote) {
+function noteToTrash(indexNote) {
   let trashNote = notes.splice(indexNote, 1);
-  trashNotes.push(trashNote);
+  trashNotes.push(trashNote[0]);
+  let trashNoteTitle = notesTitles.splice(indexNote, 1);
+  trashNotesTitles.push(trashNoteTitle[0]);
+
+  renderNotes();
+  renderTrashNotes();
+}
+
+function deleteNote(indexTrashNote) {
+  trashNotes.splice(indexTrashNote, 1);
+
   renderNotes();
   renderTrashNotes();
 }
